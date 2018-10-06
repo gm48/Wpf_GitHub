9119166ba54c4c54e3a9ef54ee9a9430fdeb6743
gm48 <mozo1900@yahoo.com>
2018-10-06 16:46:26 -0400

    Create test.cs

---
+++ Wpf_GitHub/test.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Wpf_GitHub
+{
+    /// <summary>
+    /// Interaction logic for Page1.xaml
+    /// </summary>
+    public partial class ThisOneNew
+    {
+        public ThisOneNew()
+        {
+        //  gm48   here  is g
+            InitializeComponent();
+            string gm = "Gennady Test";
+            string two = "Two as string";
+            
+            string dude = "Dude added it !"!;
+        }
+    }
+}
